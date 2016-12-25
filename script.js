b1b05ad09ed1549d780f0e58a3193697f37f6629
ccam <c.mcdonald0625@gmail.com>
2016-12-24 22:20:14 -0500

    update

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 function getData(){
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'text';
-  xhr.open('GET', 'http://api.openweathermap.org/data/2.5/forecast/city?id=5128581&APPID=19dff603cfa465a0f1e56370746d97ff&units=imperial');
+  //replace github with "https://api.wunderground.com/api/7c06c11b1ec1f632/forecast/geolookup/conditions/q/NY/New_York.json"
+  xhr.open('GET', 'https://raw.githubusercontent.com/ccam/weather/master/test.json');
 
   xhr.onload = function() {
     var data = JSON.parse(xhr.responseText);
-
+    
     renderHTML(data);
   }
   xhr.send();
 };
 
 function renderHTML(theData) {
-  var weather = document.getElementById('weatherDesc');
-  weatherLowerCase = theData.list[0].weather[0].description;
-  weather.innerHTML = weatherLowerCase.charAt(0).toUpperCase() + weatherLowerCase.slice(1);
-  
+  //for .left 
+  var pressure = document.getElementById('pressure');
+  pressure.innerHTML = theData.current_observation.pressure_in;
+
+  var vis = document.getElementById('vis');
+  vis.innerHTML = theData.current_observation.visibility_mi;
+
+  var windChill = document.getElementById('windChill');
+  windChill.innerHTML = theData.current_observation.windchill_f;
+
+  var dew = document.getElementById('dew');
+  dew.innerHTML = theData.current_observation.dewpoint_f;
+
+  var humidity = document.getElementById('humidity');
+  humidity.innerHTML = theData.current_observation.relative_humidity;
+
+  var rain = document.getElementById('rain');
+  rain.innerHTML = theData.current_observation.precip_1hr_in;
+
+  var snow = document.getElementById('snow');
+  snow.innerHTML = theData.forecast.simpleforecast.forecastday[0].snow_allday.in;
+
+  //for .middle 
+  var img = theData.forecast.txt_forecast.forecastday[0].icon_url;
+  var icon = document.getElementById('icon');
+  icon.innerHTML = '<img src="'+img+'">';
+
+  var forecast = document.getElementById('forecast');
+  forecast.innerHTML = theData.forecast.txt_forecast.forecastday[0].fcttext;
+
   var temp = document.getElementById('temp');
-  temp.innerHTML = Math.round(theData.list[0].main.temp);
-
-  var wind = document.getElementById('wind');
-  wind.innerHTML = Math.round(theData.list[0].wind.speed);
-
+  temp.innerHTML = theData.current_observation.temp_f;
+  
+  var feelsTemp = document.getElementById('feelsTemp');
+  feelsTemp.innerHTML = theData.current_observation.feelslike_f;
 };
 
 getData();
